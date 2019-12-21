f6332677c457fff9d3aaf2f8d4511c416bf0714f
jiriklepl <jiriklepl@seznam.cz>
2019-12-21 20:35:26 +0100

    the second task progress

--- HW3/Klepl_Jiri_hw3.R
+++ HW3/Klepl_Jiri_hw3.R
@@ -1,4 +1,5 @@
 library(ISLR)
+library(rpart)
 # str(Caravan)
 
 # Task 1
@@ -45,29 +46,45 @@ print(mostype.vs.moshoofd)
 
 # Task 2
 
-
 set.seed(123)
 
-Caravan.sample <- sample(nrow(Caravan))
-
-Caravan.test <- Caravan[Caravan.sample[1:1000],]
-Caravan.train <- Caravan[Caravan.sample[-c(1:1000)],]
-
-
-
-
-answered.yes <- Caravan.train[Caravan.train[,86] == "Yes",]
-answered.no <- Caravan.train[Caravan.train[,86] == "No",]
-
-
 message("Let's fold:")
-for (i in 1:6) {
-    yes.count = floor(nrow(answered.yes)/6)
-    no.count = floor(nrow(answered.no)/6)
+do.folds <- function(folds, cp){
+    Caravan.sample <- sample(nrow(Caravan))
 
-    yes.test <- answered.yes[((i-1)*yes.count+1):(i*yes.count),]
-    no.test <- answered.no[((i-1)*no.count+1):(i*no.count),]
+    Caravan.test <- Caravan[Caravan.sample[1:1000],]
+    Caravan.train <- Caravan[Caravan.sample[-c(1:1000)],]
 
-    yes.rest <- answered.yes[1:(6*yes.count),][-c(((i-1)*yes.count+1):(i*yes.count)),]
-    no.rest <- answered.no[1:(6*no.count),][-c(((i-1)*no.count+1):(i*no.count)),]
+    answered.yes <- Caravan.train[Caravan.train[,86] == "Yes",]
+    answered.no <- Caravan.train[Caravan.train[,86] == "No",]
+
+    yes.count <- floor(nrow(answered.yes)/folds)
+    no.count <- floor(nrow(answered.no)/folds)
+
+    precision.sum <- 0
+
+    for (i in 1:folds) {
+        yes.test <- answered.yes[((i-1)*yes.count+1):(i*yes.count),]
+        no.test <- answered.no[((i-1)*no.count+1):(i*no.count),]
+
+        yes.rest <- answered.yes[1:(folds*yes.count),][-c(((i-1)*yes.count+1):(i*yes.count)),]
+        no.rest <- answered.no[1:(folds*no.count),][-c(((i-1)*no.count+1):(i*no.count)),]
+
+        train <- rbind(yes.rest, no.rest)
+        test <- rbind(yes.test, no.test)
+
+        tmodel <- rpart(Purchase ~ ., train, method = "class", cp = cp)
+
+        predicted <- predict(tmodel, newdata = test, type = "class")
+
+        cm <- table(predicted = predicted, test = test$Purchase)
+
+        precision.sum <- precision.sum + cm[2,2] / sum(cm[2,])
+    }
+
+    return (precision.sum / folds)
+}
+
+for (i in 1:30) {
+    message(paste(i, do.folds(10, i * 0.0001)))
 }
